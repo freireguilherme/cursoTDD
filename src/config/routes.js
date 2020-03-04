c149337735aebe85fc9e64430c93e4cb9ce4b167
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2020-03-04 11:49:05 -0300

    configuração de rotas

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,5 +1,5 @@
 module.exports = (app) => {
   app.route('/users')
-    .get(app.routes.users.findAll)
-    .post(app.routes.users.create);
+    .get(app.routes.users.findAll) // quando receber um get, direciona para a função de listar
+    .post(app.routes.users.create); // quando receber um post, direciona para a função de criar
 };
