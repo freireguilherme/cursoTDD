1afb13f31a7a41c4e14049ac3517d1685b302d50
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2020-03-04 11:49:41 -0300

    rotas para users

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,13 @@
+// rotas para lidar com requisições para usuários
 module.exports = () => {
-  const findAll = (req, res) => {
+  const findAll = (req, res) => { // função para listar usuários
     const users = [
       { name: 'John Doe', mail: 'john@mail.com' },
     ];
     res.status(200).json(users);
   };
 
-  const create = (req, res) => {
+  const create = (req, res) => { // função para criar novo usuário
     res.status(201).json(req.body);
   };
   return { findAll, create };
