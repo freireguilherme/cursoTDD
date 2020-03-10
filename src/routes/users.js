3c10969f139239643e3d0248dc5b8ae447bd5c0f
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2020-03-10 12:14:16 -0300

    refatoração, utiliza camada de serviços

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,12 @@
 // rotas para lidar com requisições para usuários
 module.exports = (app) => {
   const findAll = (req, res) => { // função para listar usuários
-    app.db('users').select()
+    app.services.user.findAll()
       .then((result) => res.status(200).json(result));
   };
 
   const create = async (req, res) => { // função para criar novo usuário
-    const result = await app.db('users').insert(req.body, '*'); // retorna uma array
+    const result = await app.services.user.save(req.body); // retorna uma array
     res.status(201).json(result[0]); // para retornar o primeiro usuario
   };
   return { findAll, create };
