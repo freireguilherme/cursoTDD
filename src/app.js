c2f2f7987fd5877fdc1be5542bffdde7d9fd49d0
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2020-03-10 11:54:36 -0300

    logger de consultas ao db

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 const app = require('express')();
 const consign = require('consign');
+const knex = require('knex');
+// const knexlogger = require('knex-logger');
+const knexfile = require('../knexfile.js');
+
+// TODO criar chaveamento dinamico
+app.db = knex(knexfile.test);
+
+// app.use(knexlogger(app.db)); // knex-logger irá mostrar as consultas ao db nos testes
 
 consign({ cwd: 'src', verbose: false }) // consign irá organizar os arquivos
   .include('./config/middlewares.js') // incluindo o middleware (body-parser)
@@ -11,4 +19,11 @@ app.get('/', (req, res) => {
   res.status(200).send();
 });
 
+// aleternativa ao knex-logger
+/* app.db.on('query', (query) => {
+  console.log({ sql: query.sql, bindings: query.bindings ? query.bindings.join(',') : '' });
+})
+  .on('query-response', (response) => console.log(response))
+  .on('error', (error) => console.log(error));
+*/
 module.exports = app;
