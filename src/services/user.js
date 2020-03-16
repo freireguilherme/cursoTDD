258ee760b8b7482041df0b19f668e582b7951314
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2020-03-16 11:42:57 -0300

    refactor: validar e inserir campos

--- src/services/user.js
+++ src/services/user.js
@@ -1,9 +1,16 @@
 module.exports = (app) => {
-  const findAll = () => {
-    return app.db('users').select();
+  const findAll = (filter = {}) => {
+    return app.db('users').where(filter).select();
   };
 
-  const save = (user) => {
+  const save = async (user) => {
+    if (!user.name) return { error: 'Nome é atributo obrigatório' }; // validando se nome existe
+    if (!user.mail) return { error: 'Email é atributo obrigatório' }; // validando se email existe
+    if (!user.passwd) return { error: 'Senha é atributo obrigatório' }; // validando se senha existe
+
+    const userdb = await findAll({ mail: user.mail }); // validando email duplicado
+    if (userdb && userdb.length > 0) return { error: 'Já existe um usuário com esse email' };
+
     return app.db('users').insert(user, '*');
   };
 
