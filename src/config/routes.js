3eccacf546cd589d0ed5fca20357dcaf2c799ea2
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2020-03-16 12:04:27 -0300

    feat: account route

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,4 +2,7 @@ module.exports = (app) => {
   app.route('/users')
     .get(app.routes.users.findAll) // quando receber um get, direciona para a função de listar
     .post(app.routes.users.create); // quando receber um post, direciona para a função de criar
+
+  app.route('/accounts')
+    .post(app.routes.accounts.create);
 };
