fe1e896ce878e387da23011352f81ce58603a639
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2020-03-19 17:25:23 -0300

    refactor: middleware knex logger

--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -1,5 +1,7 @@
 const bodyparser = require('body-parser');
+// const knexlogger = require('knex-logger');
 
 module.exports = (app) => {
   app.use(bodyparser.json());
+  // app.use(knexlogger(app.db)); // knex-logger irá mostrar as consultas ao db nos testes
 };
