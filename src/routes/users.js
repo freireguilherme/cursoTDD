9e856ad024f1ae17eacfe86c642de464c5b30658
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2020-03-19 17:26:30 -0300

    refactor: error treatment by middleware using express params next()

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,16 +1,17 @@
 // rotas para lidar com requisições para usuários
 module.exports = (app) => {
-  const findAll = (req, res) => { // função para listar usuários
+  const findAll = (req, res, next) => { // função para listar usuários
     app.services.user.findAll()
-      .then((result) => res.status(200).json(result));
+      .then((result) => res.status(200).json(result))
+      .catch((err) => next(err));
   };
 
-  const create = async (req, res) => { // função para criar novo usuário
+  const create = async (req, res, next) => { // função para criar novo usuário
     try {
       const result = await app.services.user.save(req.body); // retorna uma array
       return res.status(201).json(result[0]); // para retornar o primeiro usuario
     } catch (err) {
-      return res.status(400).json({ error: err.message }); // se error, retorna status 400
+      return next(err); // se error, retorna status 400
     }
   };
   return { findAll, create };
