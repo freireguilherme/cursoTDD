56e8b2760b71e86f81e80420bed3ad145608e98d
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2020-03-19 16:56:12 -0300

    refactor: error treatement

--- src/services/user.js
+++ src/services/user.js
@@ -1,15 +1,17 @@
+const ValidationError = require('../errors/ValidationError');
+
 module.exports = (app) => {
   const findAll = (filter = {}) => {
     return app.db('users').where(filter).select();
   };
 
   const save = async (user) => {
-    if (!user.name) return { error: 'Nome é atributo obrigatório' }; // validando se nome existe
-    if (!user.mail) return { error: 'Email é atributo obrigatório' }; // validando se email existe
-    if (!user.passwd) return { error: 'Senha é atributo obrigatório' }; // validando se senha existe
+    if (!user.name) throw new ValidationError('Nome é atributo obrigatório'); // validando se nome existe
+    if (!user.mail) throw new ValidationError('Email é atributo obrigatório'); // validando se email existe
+    if (!user.passwd) throw new ValidationError('Senha é atributo obrigatório'); // validando se senha existe
 
     const userdb = await findAll({ mail: user.mail }); // validando email duplicado
-    if (userdb && userdb.length > 0) return { error: 'Já existe um usuário com esse email' };
+    if (userdb && userdb.length > 0) throw new ValidationError('Já existe um usuário com esse email');
 
     return app.db('users').insert(user, '*');
   };
