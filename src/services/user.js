890d27135a4172ba93c0cfb6824f43b9e557f1c7
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2020-03-24 16:07:38 -0300

    feat: passwd encrypted

--- src/services/user.js
+++ src/services/user.js
@@ -1,8 +1,18 @@
+const bcrypt = require('bcryptjs');
 const ValidationError = require('../errors/ValidationError');
 
 module.exports = (app) => {
-  const findAll = (filter = {}) => {
-    return app.db('users').where(filter).select();
+  const findAll = () => {
+    return app.db('users').select(['id', 'name', 'mail']);
+  };
+
+  const findOne = (filter = {}) => {
+    return app.db('users').where(filter).first();
+  };
+
+  const getPasswdHash = (passwd) => {
+    const salt = bcrypt.genSaltSync(10);
+    return bcrypt.hashSync(passwd, salt);
   };
 
   const save = async (user) => {
@@ -10,11 +20,14 @@ module.exports = (app) => {
     if (!user.mail) throw new ValidationError('Email é atributo obrigatório'); // validando se email existe
     if (!user.passwd) throw new ValidationError('Senha é atributo obrigatório'); // validando se senha existe
 
-    const userdb = await findAll({ mail: user.mail }); // validando email duplicado
-    if (userdb && userdb.length > 0) throw new ValidationError('Já existe um usuário com esse email');
+    const userdb = await findOne({ mail: user.mail }); // validando email duplicado
+    if (userdb) throw new ValidationError('Já existe um usuário com esse email');
 
-    return app.db('users').insert(user, '*');
+    const newUser = { ...user };
+    newUser.passwd = getPasswdHash(user.passwd);
+
+    return app.db('users').insert(newUser, ['id', 'name', 'mail']);
   };
 
-  return { findAll, save };
+  return { findAll, save, findOne };
 };
