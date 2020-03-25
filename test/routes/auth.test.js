eebf6c8718a1b523415efc6e70d7734fae0b22b3
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2020-03-25 18:38:41 -0300

    test: Nao deve autenticar usuario ou senha invalida

--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -13,3 +13,25 @@ test('Deve receber token ao logar', () => {
       expect(res.body).toHaveProperty('token');
     });
 });
+
+test('Nao deve autenticar usuario com senha errada', () => {
+  const mail = `${Date.now()}@mail.com`;
+  return app.services.user.save(
+    { name: 'Walter', mail, passwd: '123456' },
+  )
+    .then(() => request(app).post('/auth/signin')
+      .send({ mail, passwd: '654321' }))
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Usuário ou senha inválido');
+    });
+});
+
+test('Nao deve autenticar usuario com senha errada', () => {
+  return request(app).post('/auth/signin')
+    .send({ mail: 'naoExiste@mail.com', passwd: '654321' })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Usuário ou senha inválido');
+    });
+});
