87a5ce637e8f3bfefc5eae5fe4bc8b7d8ca6d430
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2020-03-25 18:24:43 -0300

    feat auth post route

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,4 +1,7 @@
 module.exports = (app) => {
+  app.route('/auth/signin')
+    .post(app.routes.auth.signin);
+
   app.route('/users')
     .get(app.routes.users.findAll) // quando receber um get, direciona para a função de listar
     .post(app.routes.users.create); // quando receber um post, direciona para a função de criar
