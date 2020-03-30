f4256e8350f256158fe7b40ee54dfc05b8d4bc6b
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2020-03-30 20:13:04 -0300

    refactor: new way to router findAll and create

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,18 +1,22 @@
 // rotas para lidar com requisições para usuários
+const express = require('express');
+
 module.exports = (app) => {
-  const findAll = (req, res, next) => { // função para listar usuários
+  const router = express.Router();
+
+  router.get('/', (req, res, next) => { // função para listar usuários
     app.services.user.findAll()
       .then((result) => res.status(200).json(result))
       .catch((err) => next(err));
-  };
+  });
 
-  const create = async (req, res, next) => { // função para criar novo usuário
+  router.post('/', async (req, res, next) => { // função para criar novo usuário
     try {
       const result = await app.services.user.save(req.body); // retorna uma array
       return res.status(201).json(result[0]); // para retornar o primeiro usuario
     } catch (err) {
       return next(err); // se error, retorna status 400
     }
-  };
-  return { findAll, create };
+  });
+  return router;
 };
