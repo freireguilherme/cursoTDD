43c030a081e0b0b96702d24810fa428d0bf8722e
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2020-03-30 20:12:13 -0300

    refactor: new way to router sign in and sign up

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,14 @@
 const jwt = require('jwt-simple');
+const express = require('express');
 const bcrypt = require('bcryptjs');
 const ValidationError = require('../errors/ValidationError');
 
 const secret = 'Segredo!';
 
 module.exports = (app) => {
-  const signin = (req, res, next) => {
+  const router = express.Router();
+
+  router.post('/signin', (req, res, next) => {
     app.services.user.findOne({ mail: req.body.mail })
       .then((user) => {
         if (!user) throw new ValidationError('Usuário ou senha inválido');
@@ -19,7 +22,16 @@ module.exports = (app) => {
           res.status(200).json({ token });
         } else throw new ValidationError('Usuário ou senha inválido');
       }).catch((err) => next(err));
-  };
+  });
 
-  return { signin };
+  router.post('/signup', async (req, res, next) => { // função para criar novo usuário
+    try {
+      const result = await app.services.user.save(req.body); // retorna uma array
+      return res.status(201).json(result[0]); // para retornar o primeiro usuario
+    } catch (err) {
+      return next(err); // se error, retorna status 400
+    }
+  });
+
+  return router;
 };
