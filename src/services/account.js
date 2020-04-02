325b22af293862bc1dd04c9d4c9deba035619381
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2020-04-02 17:55:31 -0300

    feat: agora lida com error de conta duplicada

--- src/services/account.js
+++ src/services/account.js
@@ -1,17 +1,20 @@
 const ValidationError = require('../errors/ValidationError');
 
 module.exports = (app) => {
+  const find = (filter = {}) => {
+    return app.db('accounts').where(filter).first();
+  };
+
   const save = async (account) => {
     if (!account.name) throw new ValidationError('Nome é um atributo obrigatório');
+
+    const accDB = await find({ name: account.name, user_id: account.user_id });
+    if (accDB) throw new ValidationError('Já existe uma conta com esse nome');
     return app.db('accounts').insert(account, '*');
   };
 
   const findAll = (userId) => {
     return app.db('accounts').where({ user_id: userId });
-  };
-
-  const find = (filter = {}) => {
-    return app.db('accounts').where(filter).first();
   };
 
   const update = (id, account) => {
