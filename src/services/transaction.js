1763dc1ed0ee912e3b0145aaecafb492f7ee4ef7
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2020-04-06 18:03:55 -0300

    feat: update method added

--- src/services/transaction.js
+++ src/services/transaction.js
@@ -17,6 +17,12 @@ module.exports = (app) => {
       .insert(transaction, '*');
   };
 
+  const update = (id, transaction) => {
+    return app.db('transactions')
+      .where({ id })
+      .update(transaction, '*');
+  };
 
-  return { find, save, findOne };
+
+  return { find, save, findOne, update };
 };
