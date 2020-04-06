b4177e9dd4a2d4fa3d898396e8c644577291bc34
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2020-04-06 18:03:25 -0300

    feat: router.put added

--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -14,9 +14,16 @@ module.exports = (app) => {
       .then((result) => res.status(201).json(result[0]))
       .catch((err) => next(err));
   });
+
   router.get('/:id', (req, res, next) => {
     app.services.transaction.findOne({ id: req.params.id })
       .then((result) => res.status(200).json(result))
+      .catch((err) => next(err));
+  });
+
+  router.put('/:id', (req, res, next) => {
+    app.services.transaction.update(req.params.id, req.body)
+      .then((result) => res.status(200).json(result[0]))
       .catch((err) => next(err));
   });
   return router;
