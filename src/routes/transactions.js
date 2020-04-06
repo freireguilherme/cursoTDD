b6ed60c1afbf1328ac6839dd948b64e7ecac2ec0
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2020-04-05 22:23:46 -0300

    feat: router.get by id method add

--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -14,6 +14,10 @@ module.exports = (app) => {
       .then((result) => res.status(201).json(result[0]))
       .catch((err) => next(err));
   });
-
+  router.get('/:id', (req, res, next) => {
+    app.services.transaction.findOne({ id: req.params.id })
+      .then((result) => res.status(200).json(result))
+      .catch((err) => next(err));
+  });
   return router;
 };
