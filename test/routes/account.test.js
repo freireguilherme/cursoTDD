5863efb51156ea1d5409ef5e86e62cf74529a7a7
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2020-04-07 22:54:53 -0300

    test: change for improve test speed

--- test/routes/account.test.js
+++ test/routes/account.test.js
@@ -6,7 +6,7 @@ const MAIN_ROUTE = '/v1/accounts';
 let user;
 let user2;
 
-beforeEach(async () => {
+beforeAll(async () => {
   const res = await app.services.user.save({ name: 'User Account', mail: `${Date.now()}@mail.com`, passwd: '123456' });
   user = { ...res[0] };
   user.token = jwt.encode(user, 'Segredo!');
@@ -47,7 +47,9 @@ test('Não deve inserir uma conta de nome duplicado para o mesmo usuario', () =>
     });
 });
 
-test('Deve listar apenas as contas do usuario', () => {
+test('Deve listar apenas as contas do usuario', async () => {
+  await app.db('transactions').del();
+  await app.db('accounts').del();
   return app.db('accounts').insert([
     { name: 'Acc User #1', user_id: user.id },
     { name: 'Acc User #2', user_id: user2.id },
