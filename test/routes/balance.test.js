b0af0d193c6a7e59ae7f55f70eccef7f8448127b
freireguilherme <guilhermemagalhaesfreire22@gmail.com>
2020-04-23 00:57:08 -0300

    test:Devo calcular saldo

--- test/routes/balance.test.js
+++ test/routes/balance.test.js
@@ -6,6 +6,7 @@ const MAIN_ROUTE = '/v1/balance';
 const ROUTE_TRANSACTION = '/v1/transactions';
 const ROUTE_TRANSFER = '/v1/transfers';
 const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEwMTAwIiwibmFtZSI6IlVzZXIgIzMiLCJtYWlsIjoidXNlcjNAbWFpbC5jb20ifQ.kq65elChkjODMW_PAWGloR-CjsL1dB9eyyT6a4hZGas';
+const TOKEN_GERAL = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEwMTAyIiwibmFtZSI6IlVzZXIgIzUiLCJtYWlsIjoidXNlcjVAbWFpbC5jb20ifQ.x3AR3oMTkHahePVP-n5LoWmBeqKJ88NFvCz7GVuvSs0';
 
 beforeAll(async () => {
   await app.db.seed.run();
@@ -21,36 +22,28 @@ describe('Ao calcular o saldo do usuario...', () => {
       });
   });
 
-  test('Deve adicionar valores de entrada', () => {
-    return request(app).post(ROUTE_TRANSACTION)
+  test('Deve adicionar valores de entrada', async () => {
+    await request(app).post(ROUTE_TRANSACTION)
       .set('authorization', `bearer ${TOKEN}`)
-      .send({ description: '1', date: new Date(), ammount: 100, type: 'I', acc_id: 10100, status: true, transfer_id: 10001 })
-      .then(() => {
-        return request(app).get(MAIN_ROUTE)
-          .set('authorization', `bearer ${TOKEN}`)
-          .then((res) => {
-            expect(res.status).toBe(200);
-            expect(res.body).toHaveLength(1);
-            expect(res.body[0].id).toBe(10100);
-            expect(res.body[0].sum).toBe('100.00');
-          });
-      });
+      .send({ description: '1', date: new Date(), ammount: 100, type: 'I', acc_id: 10100, status: true });
+    const res = await request(app).get(MAIN_ROUTE)
+      .set('authorization', `bearer ${TOKEN}`);
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveLength(1);
+    expect(res.body[0].id).toBe(10100);
+    expect(res.body[0].sum).toBe('100.00');
   });
 
-  test('Deve subtrair valores de saida', () => {
-    return request(app).post(ROUTE_TRANSACTION)
+  test('Deve subtrair valores de saida', async () => {
+    await request(app).post(ROUTE_TRANSACTION)
       .send({ description: '1', date: new Date(), ammount: 200, type: 'O', acc_id: 10100, status: true })
-      .set('authorization', `bearer ${TOKEN}`)
-      .then(() => {
-        return request(app).get(MAIN_ROUTE)
-          .set('authorization', `bearer ${TOKEN}`)
-          .then((res) => {
-            expect(res.status).toBe(200);
-            expect(res.body).toHaveLength(1);
-            expect(res.body[0].id).toBe(10100);
-            expect(res.body[0].sum).toBe('-100.00');
-          });
-      });
+      .set('authorization', `bearer ${TOKEN}`);
+    const res = await request(app).get(MAIN_ROUTE)
+      .set('authorization', `bearer ${TOKEN}`);
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveLength(1);
+    expect(res.body[0].id).toBe(10100);
+    expect(res.body[0].sum).toBe('-100.00');
   });
 
   test('Nao deve considerar transacoes pendentes', () => {
@@ -155,4 +148,17 @@ describe('Ao calcular o saldo do usuario...', () => {
           });
       });
   });
+});
+
+test('Deve calcular saldo das contas do usuario', () => {
+  return request(app).get(MAIN_ROUTE)
+    .set('authorization', `bearer ${TOKEN_GERAL}`)
+    .then((res) => {
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveLength(2);
+      expect(res.body[0].id).toBe(10104);
+      expect(res.body[0].sum).toBe('162.00');
+      expect(res.body[1].id).toBe(10105);
+      expect(res.body[1].sum).toBe('-248.00');
+    });
 });
